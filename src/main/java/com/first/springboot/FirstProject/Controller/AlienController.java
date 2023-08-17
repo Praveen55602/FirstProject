213b6cb945d4447e4dc91875b71a65c986ce72fb
Praveen <praveensingh55602@gmail.com>
2023-08-17 16:58:05 +0530

    implementing rest in the project

--- src/main/java/com/first/springboot/FirstProject/Controller/AlienController.java
+++ src/main/java/com/first/springboot/FirstProject/Controller/AlienController.java
@@ -4,34 +4,37 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.first.springboot.FirstProject.Dao.AlienRepo;
 import com.first.springboot.FirstProject.Model.Alien;
 
-@Controller
+@RestController
 @RequestMapping("/aliens") // all the url paths starting with /home will be redirected to this controller
 public class AlienController {
     @Autowired
     AlienRepo repo;
 
-    @RequestMapping("") // "/aliens" will be redirected here
+    @GetMapping("") // "/aliens" will be redirected here
     public String AlienHome() {
         return "alien";
     }
 
-    @RequestMapping("/add")
-    public String addAlien(Alien alien) {
+    @PostMapping("/add")
+    public Alien addAlien(Alien alien) {
         repo.save(alien);
-        return "alien";
+        return alien;
     }
 
-    @RequestMapping("/getalien")
+    @GetMapping("/getalien")
     public ModelAndView getAlien(@RequestParam(name = "aid") Integer aid) {
         Alien res = repo.findById(aid).orElse(new Alien());
         ModelAndView mv = new ModelAndView("showAlien");
@@ -39,7 +42,7 @@ public class AlienController {
         return mv;
     }
 
-    @RequestMapping("/getaliens")
+    @GetMapping("/getaliens")
     @ResponseBody // as we are just returning response from this. This tag tell that I'll not go
                   // to any view just return a response
     public List<Alien> getAliens() {// now the all the aliens will be returned and the response will automatically
@@ -47,7 +50,7 @@ public class AlienController {
         return repo.findAll();
     }
 
-    @RequestMapping("/getaliens/{aid}")
+    @GetMapping("/getaliens/{aid}")
     @ResponseBody
     // since findbyid may return null that's why here java forces us to return
     // optional
@@ -56,7 +59,7 @@ public class AlienController {
     }
 
     // there is no UI for update and delete just pass everything as request params
-    @RequestMapping("/updatealien")
+    @GetMapping("/updatealien")
     public ModelAndView updateAlien(Alien alien) {
         Alien res = repo.findById(alien.aid).orElse(null);
         if (res != null) {
@@ -70,13 +73,14 @@ public class AlienController {
         return mv;
     }
 
-    @RequestMapping("/deletealien")
-    public String removeAlien(@RequestParam(name = "aid") Integer aid) {
+    @DeleteMapping("/deletealien/{aid}")
+    public Alien removeAlien(@PathVariable int aid) {
+        Alien a = repo.findById(aid).orElse(new Alien());
         repo.deleteById(aid);
-        return "alien";
+        return a;
     }
 
-    @RequestMapping("/getbylang")
+    @GetMapping("/getbylang")
     public String getByLang(@RequestParam(name = "lang") String lang) {
         System.out.println(repo.findByLang(lang));
         System.out.println(repo.findByAidGreaterThan(1));
